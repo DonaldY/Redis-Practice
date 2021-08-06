4986fcc1d64c4217304a1e41832dd6c9271fb361
Donald <448641125@qq.com>
2021-08-06 19:22:30 +0800

    update jedis demo

--- src/test/java/com/donaldy/redispractice/JedisTest.java
+++ src/test/java/com/donaldy/redispractice/JedisTest.java
@@ -617,4 +617,60 @@ public class JedisTest {
         System.out.println("你自己刷朋友圈，看到自己发的朋友圈被" + momentLikeUsersCount + "个人点赞了，点赞的用户为：" + momentLikeUsers);
     }
 
+    // ===================== 投票 =====================
+    /**
+     * 投票
+     * @param userId 用户Id
+     * @param voteItemId 投票事项
+     */
+    private void vote(long userId, long voteItemId) {
+        jedis.sadd("vote_item_users::" + voteItemId, String.valueOf(userId));
+    }
+
+    /**
+     * 检查用户对投票项是否投过票
+     * @param userId 用户Id
+     * @param voteItemId 投票事项
+     * @return 是否
+     */
+    private boolean hasVoted(long userId, long voteItemId) {
+        return jedis.sismember("vote_item_users::" + voteItemId, String.valueOf(userId));
+    }
+
+    /**
+     * 获取一个投票项被哪些人投票了
+     * @param voteItemId 投票事项
+     * @return 列表
+     */
+    private Set<String> getVoteItemUsers(long voteItemId) {
+        return jedis.smembers("vote_item_users::" + voteItemId);
+    }
+
+    /**
+     * 获取一个投票项被多少人投票了
+     * @param voteItemId 投票事项
+     * @return 总数
+     */
+    private long getVoteItemUsersCount(long voteItemId) {
+        return jedis.scard("vote_item_users::" + voteItemId);
+    }
+
+    @Test
+    public void testVote() {
+
+        // 定义用户id
+        long userId = 1;
+        // 定义投票项id
+        long voteItemId = 110;
+
+        // 进行投票
+        vote(userId, voteItemId);
+        // 检查我是否投票过
+        boolean hasVoted = hasVoted(userId, voteItemId);
+        System.out.println("用户查看自己是否投票过：" +(hasVoted ? "是" : "否"));
+        // 归票统计
+        Set<String> voteItemUsers = getVoteItemUsers(voteItemId);
+        long voteItemUsersCount = getVoteItemUsersCount(voteItemId);
+        System.out.println("投票项有哪些人投票：" + voteItemUsers + "，有几个人投票：" + voteItemUsersCount);
+    }
 }
